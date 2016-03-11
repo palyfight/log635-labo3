b5cb16c1f105114a67072397552e71f06825c7c2
Chris Bama <AK68250@ens.etsmtl.ca>
2016-03-11 10:51:53 -0500

    initial work

---
+++ csv_reader.py
@@ -0,0 +1,36 @@
+import csv
+
+
+
+class LearningStrategy:
+	def __init__(self, algorithm=None):
+		if(algorithm):
+			self.algorithm = algorithm()
+
+	def learn(self,data):
+		if(self.algorithm):
+			return self.algorithm.learn(data)
+
+class DeepLearning:
+	def learn(self,data):
+		return data
+
+class Genetics:
+	def learn(self,data):
+		return data
+
+class Util:
+	def readInterpretData(filename):
+		with open(filename) as file:
+			reader = csv.DictReader(file,delimiter=';')
+			data = []
+			for row in reader:
+				data.append(row)
+		return data
+
+def main():
+	data = Util.readInterpretData('learning_dataset.csv')
+	algo = LearningStrategy(algorithm=Genetics)
+	proc_data = algo.learn(data)
+	print(proc_data)
+if __name__ == "__main__":main()
